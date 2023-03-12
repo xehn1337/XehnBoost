a611719499e096c24ae6d2fde62c003857132bf7
xehn1337 <ximkai12@gmail.com>
2023-03-12 17:37:49 +0100

    Source

---
+++ XehnBoost/Views/Pages/Debloat.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XehnBoost.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Debloat.xaml
+    /// </summary>
+    public partial class Debloat : Page
+    {
+        public Debloat()
+        {
+            InitializeComponent();
+        }
+
+        private void OldContext(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\\InprocServer32", "", "");
+            string message = "Windows 10 Style Context Menu Applied!";
+            MessageBox.Show(message);
+            Process p = new Process();
+            foreach (Process exe in Process.GetProcesses())
+            {
+                if (exe.ProcessName == "explorer")
+                    exe.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        private void NewContext(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo contextdel = new ProcessStartInfo("WT.exe");
+            contextdel.UseShellExecute = true;
+            contextdel.Arguments = "REG DELETE \"HKCU\\Software\\Classes\\CLSID\\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\" /f";
+            Process.Start(contextdel);
+            string message = "Windows 11 Style Context Menu Applied!";
+            MessageBox.Show(message);
+        }
+
+        private void HiddenFolders(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "Hidden", "00000001", RegistryValueKind.DWord);
+            string message = "Hidden Folders Visible now!";
+            MessageBox.Show(message);
+        }
+
+        private void HideHiddenFolders(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "Hidden", "00000000", RegistryValueKind.DWord);
+            string message = "Hidden Folders Invisible now!";
+            MessageBox.Show(message);
+        }
+
+        private void Extensions(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "HideFileExt", "00000000", RegistryValueKind.DWord);
+            string message = "File Extensions visible now!";
+            MessageBox.Show(message);
+        }
+
+        private void HideExtensions(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "HideFileExt", "00000001", RegistryValueKind.DWord);
+            string message = "File Extensions invisible now!";
+            MessageBox.Show(message);
+        }
+
+        private void EnableNoti(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\PushNotifications", "ToastEnabled", "00000001", RegistryValueKind.DWord);
+
+            ProcessStartInfo restartService = new ProcessStartInfo("powershell.exe");
+            restartService.Arguments = "Restart-Service -DisplayName 'Windows Push Notifications User Service*'";
+            restartService.CreateNoWindow = true;
+            Process.Start(restartService);
+            string message = "Notifications Enabled!";
+            MessageBox.Show(message);
+        }
+
+        private void DisableNoti(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\PushNotifications", "ToastEnabled", "00000000", RegistryValueKind.DWord);
+
+            ProcessStartInfo restartService = new ProcessStartInfo("powershell.exe");
+            restartService.Arguments = "Restart-Service -DisplayName 'Windows Push Notifications User Service*'";
+            restartService.CreateNoWindow = true;
+            Process.Start(restartService);
+            string message = "Notifications Disabled!";
+            MessageBox.Show(message);
+        }
+
+        private void ClipON(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Clipboard", "EnableClipboardHistory", "00000001", RegistryValueKind.DWord);
+            string message = "Clipboard History Enabled!";
+            MessageBox.Show(message);
+        }
+
+        private void ClipOFF(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Clipboard", "EnableClipboardHistory", "00000000", RegistryValueKind.DWord);
+            string message = "Clipboard History Disabled!";
+            MessageBox.Show(message);
+        }
+
+        private void TransparencyON(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "EnableTransparency", "00000001", RegistryValueKind.DWord);
+            string message = "Transparency Effects Enabled!";
+            MessageBox.Show(message);
+        }
+
+        private void TransparencyOFF(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "EnableTransparency", "00000000", RegistryValueKind.DWord);
+            string message = "Transparency Effects Disabled!";
+            MessageBox.Show(message);
+        }
+
+        private void Smalltaskbar(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarSi", "00000000", RegistryValueKind.DWord);
+            string message = "Small Taskbar Size Applied!";
+            MessageBox.Show(message);
+            Process p = new Process();
+            foreach (Process exe in Process.GetProcesses())
+            {
+                if (exe.ProcessName == "explorer")
+                    exe.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        private void Midtaskbar(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarSi", "00000001", RegistryValueKind.DWord);
+            string message = "Medium Taskbar Size Applied!";
+            MessageBox.Show(message);
+            Process p = new Process();
+            foreach (Process exe in Process.GetProcesses())
+            {
+                if (exe.ProcessName == "explorer")
+                    exe.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        private void Largetaskbar(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarSi", "00000002", RegistryValueKind.DWord);
+            string message = "Large Taskbar Size Applied!";
+            MessageBox.Show(message);
+            Process p = new Process();
+            foreach (Process exe in Process.GetProcesses())
+            {
+                if (exe.ProcessName == "explorer")
+                    exe.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        private void WidgetsOn(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarDa", "00000001", RegistryValueKind.DWord);
+            string message = "Widgets Button Shown!";
+            MessageBox.Show(message);
+            
+        }
+
+        private void WidgetsOFF(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarDa", "00000000", RegistryValueKind.DWord);
+            string message = "Widgets Button Hidden!";
+            MessageBox.Show(message);
+           
+        }
+
+        private void ChatsOn(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarMn", "00000001", RegistryValueKind.DWord);
+            string message = "Chat Button Shown!";
+            MessageBox.Show(message);
+        }
+
+        private void ChatsOFF(object sender, RoutedEventArgs e)
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", "TaskbarMn", "00000000", RegistryValueKind.DWord);
+            string message = "Chat Button Hidden!";
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ XehnBoost/Views/Pages/Downloads.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace XehnBoost.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Downloads.xaml
+    /// </summary>
+    public partial class Downloads : Page
+    {
+        public Downloads()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://raw.githubusercontent.com/xehn1337/XehnBoost/main/v2/DownloadNvProfile.bat", "DownloadNvProfile.bat");
+            File.SetAttributes(@"DownloadNvProfile.bat", File.GetAttributes(@"DownloadNvProfile.bat") | FileAttributes.Hidden);
+
+            Process process = new Process();
+            // Start Process properties.
+            process.StartInfo.FileName = "DownloadNvProfile.bat";
+            process.StartInfo.Arguments = "-n";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit(); // Wait  for the process to exit.
+
+            // Delete file after it finishes
+            if (File.Exists(@"DownloadNvProfile.bat"))
+            {
+                File.Delete(@"DownloadNvProfile.bat");
+            }
+
+            string message = "Nvidia Control Panel Tweaks Applied!";
+            MessageBox.Show(message);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            // Delete file after it finishes
+            if (File.Exists(@"NVCleanstall_1.13.0.exe"))
+            {
+                File.Delete(@"NVCleanstall_1.13.0.exe");
+            }
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/977950699650441277/979097227618291812/NVCleanstall_1.13.0.exe", "NVCleanstall_1.13.0.exe");
+            File.SetAttributes(@"NVCleanstall_1.13.0.exe", File.GetAttributes(@"NVCleanstall_1.13.0.exe") | FileAttributes.Hidden);
+
+            Process process = new Process();
+            // Start Process properties.
+            process.StartInfo.FileName = "NVCleanstall_1.13.0.exe";
+            process.StartInfo.Arguments = "-n";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit(); // Wait  for the process to exit.
+
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/977950699650441277/979097250464694292/MSI_util_v3.exe", "MSI_util_v3.exe");
+            File.SetAttributes(@"MSI_util_v3.exe", File.GetAttributes(@"MSI_util_v3.exe") | FileAttributes.Hidden);
+
+            Process process = new Process();
+            // Start Process properties.
+            process.StartInfo.FileName = "MSI_util_v3.exe";
+            process.StartInfo.Arguments = "-n";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit(); // Wait  for the process to exit.
+
+            // Delete file after it finishes
+            if (File.Exists(@"MSI_util_v3.exe"))
+            {
+                File.Delete(@"MSI_util_v3.exe");
+            }
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/977950699650441277/979097265740333157/Autoruns.exe", "Autoruns.exe");
+            File.SetAttributes(@"Autoruns.exe", File.GetAttributes(@"Autoruns.exe") | FileAttributes.Hidden);
+
+            Process process = new Process();
+            process.StartInfo.FileName = "Autoruns.exe";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit(); // Wait  for the process to exit.
+
+            // Delete file after it finishes
+            if (File.Exists(@"Autoruns.exe"))
+            {
+                File.Delete(@"Autoruns.exe");
+            }
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id Discord.Discord";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void Install7Zip(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id 7zip.7zip";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void installVLC(object sender, RoutedEventArgs e)
+        {
+            
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id VideoLAN.VLC";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void installChrome(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id Google.Chrome";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void InstallFirefox(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id Mozilla.Firefox";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void InstallSteam(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id Valve.Steam";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void InstallEpic(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id EpicGames.EpicGamesLauncher";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void InstallOpera(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id Opera.OperaGX";
+            Process.Start(installProgram).WaitForExit();
+        }
+
+        private void InstallBrave(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo installProgram = new ProcessStartInfo("Powershell.exe");
+            installProgram.UseShellExecute = false;
+            installProgram.Arguments = "winget install -e --id BraveSoftware.BraveBrowser";
+            Process.Start(installProgram).WaitForExit();
+        }
+    }
+}
